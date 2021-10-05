5de5c03c1e7ee3bfc8111b65636f672c256d5389
bjorn <bjornvdhorst10@gmail.com>
2021-10-05 15:33:12 +0200

    added install for linux

--- install.py
+++ install.py
@@ -2,20 +2,12 @@ import os
 import sys
 import subprocess
 
-# if(sys.platform=="win32"):
-#     os.system('cmd /k "venv\Scripts\\activate && flask run"')
-# elif(sys.platform=="linux" or sys.platform =="linux2"):
-#     os.system("source env/bin/activate")
-#     os.system("flask run")
 
 if (sys.platform == "win32"):
-    import subprocess
-
-    # This command could have multiple commands separated by a new line \n
-    # some_command = "cmd /c ping www.google.com"
     some_command = "python -m venv venv && venv\Scripts\\activate && py -m pip install flask && py -m pip install flask-mysqldb && pip install python-dotenv"
     p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     print(output)
-
+elif(sys.platform=="linux" or sys.platform =="linux2"):
+    os.system("python -m venv venv && venv\Scripts\\activate && py -m pip install flask && py -m pip install flask-mysqldb && pip install python-dotenv")
 
