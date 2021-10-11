ae2e89632f0998ad2575d71d001f256d0fd3ecf9
bjorn <bjornvdhorst10@gmail.com>
2021-10-11 15:07:28 +0200

    new login

--- install.py
+++ install.py
@@ -4,7 +4,9 @@ import subprocess
 
 
 if (sys.platform == "win32"):
-    some_command = "python -m venv venv && venv\Scripts\\activate && py -m pip install flask && py -m pip install flask-mysqldb && pip install python-dotenv"
+    #some_command = "python -m venv venv && venv\Scripts\\activate && py -m pip install flask && py -m pip install flask-mysqldb && pip install python-dotenv"
+    some_command = "python -m venv venv && venv\Scripts\\activate && py -m pip install flask python-dotenv flask-sqlalchemy flask-login"
+
     p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     print(output)
