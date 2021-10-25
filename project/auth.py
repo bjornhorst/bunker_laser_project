b6e6f2b6a5f7bdfe2c98145f5a74a157719c654d
bjorn <bjornvdhorst10@gmail.com>
2021-10-25 09:00:51 +0200

    geen idee wat er gebeurd is

--- project/auth.py
+++ project/auth.py
@@ -86,3 +86,8 @@ def reset_password(token):
         flash('Your password has been reset.')
         return redirect(url_for('auth.login'))
     return render_template('auth/reset_password.html', form=form)
+
+@auth.route('/bigVideo')
+def bigVid():
+
+    return render_template('bigVideo.html')
