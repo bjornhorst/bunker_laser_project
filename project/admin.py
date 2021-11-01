11fac69d1ee2f7f1c715f0d85b3bb8a10fce2a62
Sjoerd Huisman <s.huisman@mmc-itsolutions.nl>
2021-11-01 09:34:40 +0100

    opmaak admin panel

--- project/admin.py
+++ project/admin.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 from . import db
+from .models import Video
 
 admin = Blueprint('admin', __name__)
 
 @admin.route("/admin", methods=['GET'])
 @login_required
 def adminPanel():
-    return render_template('admin/admin.html');
+    video_data = Video.query.all()
+    return render_template('admin/admin.html', video_data=video_data);
 
 @admin.route("/admin/create", methods=['GET'])
 @login_required
