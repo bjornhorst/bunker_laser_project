a35a52784bb962ea2866f9e4bfdf34c8c101feb7
Dion03 <61456914+Dion03@users.noreply.github.com>
2021-11-22 12:44:44 +0100

    favicon fix

--- install.py
+++ install.py
@@ -4,10 +4,9 @@ import subprocess
 
 if (sys.platform == "win32"):
     #some_command = "python -m venv venv && venv\Scripts\\activate && py -m pip install flask && py -m pip install flask-mysqldb && pip install python-dotenv"
-    some_command = "py -m venv venv && venv\Scripts\\activate && py -m pip install flask python-dotenv flask-sqlalchemy flask-login flask-mail pyjwt flask-wtf flask-migrate email_validator"
+    some_command = "python -m venv venv && venv\Scripts\\activate && python -m pip install flask python-dotenv flask-sqlalchemy flask-login flask-mail pyjwt flask-wtf flask-migrate email_validator"
     p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     print(output)
-elif(sys.platform=="linux" or sys.platform =="linux2"):
+elif(sys.platform == "linux" or sys.platform == "linux2"):
     os.system("python3 -m venv venv && venv/bin/activate && py -m pip install flask && py -m pip install flask-mysqldb && pip install python-dotenv")
-
