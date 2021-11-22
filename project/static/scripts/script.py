cc26deef1f05f79c08ba1767de321871561c775d
bjorn <bjornvdhorst10@gmail.com>
2021-11-22 09:54:25 +0100

    you can pause a script now

---
+++ project/static/scripts/script.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import os, sys, signal
+import time
+import csv
+pid=os.getpid ()
+with open ('pid.csv','w') as csvfile:
+    wr=csv.writer(csvfile)
+    wr.writerow ( [ 'WORK1',pid])
+while (True):
+    print ('Programmatic Trading')
+    time.sleep (1)
+print("This process has the PID", os.getpid())
+# while True:
+#     for i in range(10000):
+#         sys.stdout.write('\r')
+#         sys.stdout.write(str(i))
+#         sys.stdout.flush()
